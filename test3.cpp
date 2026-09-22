4e8dc71a111ecf1475f7bd9f197a796126723c51
pinealhua <1054232008@qq.com>
2026-09-22 19:41:45 +0800

    指针学习更新代码

--- test3.cpp
+++ test3.cpp
@@ -1,16 +1,27 @@
 //指针就是能保存地址的变量
 // double* ptr = &speed;  ptr 是一个“指向 double 的指针”，现在它指向 speed
-
+// ptr就是地址，*ptr是变量本身
 #include<iostream>
-
+double robot_speed = 3.0;
+void energencystop(double* speed);
 int main()
 {
-    int speed = 2;
-    int* ptr = &speed;
-    std::cout<<speed<<std::endl;
-    std::cout<<&speed<<std::endl;
-    std::cout<<ptr<<std::endl;
-    std::cout<<*ptr<<std::endl;
-    std::cout<<&ptr<<std::endl;
+   
+    
+    energencystop(nullptr);
+
+    // *ptr = 6;
+    std::cout<<robot_speed<<std::endl;
+  
+    
+
     return 0;
+
 }
+void energencystop(double* speed)
+{
+    if(speed!=nullptr)
+    *speed=0;
+
+}
+//if (ptr != nullptr && *ptr > 0)  如果ptr不是空指针，那么才会看后面。避免空指针带来的野指针
